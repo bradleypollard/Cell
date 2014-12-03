1e339bb02297d46e5e8fee9f0937ebffb739e328
bradleypollard <bp912@ic.ac.uk>
2014-12-03 19:09:59 +0000

    Gameplay or something

---
+++ Assets/Scripts/AdvancedFoodScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdvancedFoodScript : MonoBehaviour
+{
+  public GameObject fud;
+
+  // Use this for initialization
+  void Start()
+  {
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // Player click
+    if (Input.GetMouseButtonDown(0))
+    {
+      Vector3 v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+      v3 = Camera.main.ScreenToWorldPoint(v3);
+      GameObject f = (GameObject)Instantiate(fud, v3, Quaternion.identity);
+      AdvancedLife.foods.Add(f);
+
+      foreach (GameObject o in AdvancedLife.cells)
+      {
+        AdvancedLife l = o.GetComponent<AdvancedLife>();
+        l.updated = false;
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/AdvancedLife.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdvancedLife : MonoBehaviour
+{
+
+  public GameObject cell;
+  public float speed = 1f;
+  public float maxTTL = 5f;
+  public int hunger = 3;
+  public bool updated = true;
+
+  public static ArrayList foods;
+  public static ArrayList cells;
+
+  private int food = 0;
+  private float ttl = 0f;
+  private float minv = 1000000;
+  private Vector3 minPos = Vector3.zero;
+  private GameObject minObj = null;
+
+  // Use this for initialization
+  void Start()
+  {
+    ttl = maxTTL;
+    if (foods == null)
+    {
+      foods = new ArrayList();
+    }
+    if (cells == null)
+    {
+      cells = new ArrayList();
+      cells.Add(gameObject);
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    /* Death by old age */
+    ttl -= Time.deltaTime;
+    if (ttl < 0)
+    {
+      cells.Remove(gameObject);
+      Destroy(gameObject);
+    }
+
+    /* Division */
+    if (food > hunger)
+    {
+      cells.Add((GameObject)Instantiate(cell, transform.position, transform.rotation));
+      food = 0;
+      ttl = maxTTL;
+    }
+
+    /* Movement */
+    if (!updated)
+    {
+      // Calculate target once only
+      minv = 1000000;
+      minPos = Vector3.zero;
+      foreach (GameObject o in foods)
+      {
+        Vector3 v = transform.position - o.transform.position;
+        float d = v.sqrMagnitude;
+        if (d < minv)
+        {
+          minv = d;
+          minPos = o.transform.position;
+          minObj = o;
+        }
+      }
+      updated = true;
+    }
+    // Apply movement
+    if (minObj != null)
+    {
+      Vector3 dir = minPos - transform.position;
+      rigidbody2D.AddForce(speed * dir.normalized);
+    }
+    else
+    {
+      updated = false;
+    }
+  }
+
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject.tag == "Food")
+    {
+      /* Eating */
+      foods.Remove(other.gameObject);
+      Destroy(other.gameObject);
+      food++;
+    }
+    else if (other.gameObject.tag == "Kill")
+    {
+      cells.Remove(gameObject);
+      Destroy(gameObject);
+    }
+  }
+}
